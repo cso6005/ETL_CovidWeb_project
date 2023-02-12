99c067db8440cb8894e9c82ee0d84e23d21956b1
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-02-12 17:45:04 +0900

    Update dbutil.py

--- ETLproject_final/dbutil.py
+++ ETLproject_final/dbutil.py
@@ -4,7 +4,7 @@ import pymysql
  
 def getConnect():
     conn = pymysql.connect(host='localhost',
-    port=3306, db='coviddata', user='bigdata', password='bigdata', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+    port=3306, db='coviddata', user='bigdata', password='', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
     return conn
  
 
